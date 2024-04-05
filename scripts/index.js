bcd7a2063a03bc2edc66a39836d56fbdf96cc4ab
Oleg Goloshumov <Oleggoloshumov@yandex.ru>
2024-04-05 10:45:11 +0300

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,13 @@ const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function createCard(card, deleteCallback){
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  let cardImage = cardElement.querySelector('.card__image');
-  let cardDeleteButton = cardElement.querySelector('.card__delete-button');
-  let cardTitle = cardElement.querySelector('.card__title');
-  let cardLikeButton = cardElement.querySelector('.card__like-button');
-  
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardLikeButton = cardElement.querySelector('.card__like-button');
+
   cardImage.src = card.link;
+  cardImage.alt = card.name;
   cardTitle.textContent = card.name; 
 
   cardDeleteButton.addEventListener('click', function() {
